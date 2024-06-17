8cbbc8b0fbe0b03feb817a4c025ce4703ac05287
Hrithik-Joshi <84170164+Hrithik-Joshi@users.noreply.github.com>
2024-06-17 10:24:06 +0530

    added shared about info

---
+++ NewsApp/androidApp/src/main/java/com/hrithik/dailynews/android/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.hrithik.dailynews.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hrithik.dailynews.Platform
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Platform().logSystemInfo()
+        setContent {
+            MyApplicationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+//                    GreetingView("Hello")
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar() {
+    TopAppBar(title = { Text(text = "About")})
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MyApplicationTheme {
+//        GreetingView("Hello, Android!")
+    }
+}
