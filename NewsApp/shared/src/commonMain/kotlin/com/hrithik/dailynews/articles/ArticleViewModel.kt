3335b204622c29dd5fb7dd80a8ba3e8d51a075e0
Hrithik-Joshi <84170164+Hrithik-Joshi@users.noreply.github.com>
2024-06-20 12:24:57 +0530

    modified view model

--- NewsApp/shared/src/commonMain/kotlin/com/hrithik/dailynews/articles/ArticleViewModel.kt
+++ NewsApp/shared/src/commonMain/kotlin/com/hrithik/dailynews/articles/ArticleViewModel.kt
@@ -3,6 +3,7 @@ package com.hrithik.dailynews.articles
 import com.hrithik.dailynews.BaseViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class  ArticleViewModel : BaseViewModel() {
 
@@ -10,7 +11,7 @@ class  ArticleViewModel : BaseViewModel() {
     MutableStateFlow is a state holder observable that holds a single value
     and can be updated.
     */
-    private val _articleState: MutableStateFlow<ArticleState> = MutableStateFlow(ArticleState())
+    private val _articleState: MutableStateFlow<ArticleState> = MutableStateFlow(ArticleState(loading = true))
 
     /* This is a public, read-only StateFlow property that exposes the state to observers.
     It returns the _articleState which means any changes to _articleState
@@ -18,4 +19,18 @@ class  ArticleViewModel : BaseViewModel() {
     */
     val articleState: StateFlow<ArticleState>
         get() = _articleState
+    fun getArticles(forceFetch: Boolean = false) {
+        scope.launch {
+            _articleState.emit(
+                ArticleState(
+                    loading = true,
+                    articles = _articleState.value.articles
+                )
+            )
+
+//            val fetchedArticles = useCase.getArticles(forceFetch)
+//
+//            _articleState.emit(ArticleState(articles = fetchedArticles))
+        }
+    }
 }
