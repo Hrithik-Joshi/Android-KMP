d8063aea9f65b21a53d6af2847e1e1640124b265
Hrithik-Joshi <84170164+Hrithik-Joshi@users.noreply.github.com>
2024-06-21 14:12:28 +0530

    created article services

---
+++ NewsApp/shared/src/commonMain/kotlin/com/hrithik/dailynews/articles/ArticlesService.kt
@@ -0,0 +1,20 @@
+package com.hrithik.dailynews.articles
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+
+class ArticlesService(private val httpClient: HttpClient) {
+
+    private val country = "us"
+    private val category = "techcrunch"
+    private val apiKey = "408fca3a4b3e47479110213cac57143a"
+
+    suspend fun fetchArticles(): List<ArticleRaw> {
+        val response: ArticlesResponse =
+            httpClient.get("https://newsapi.org/v2/top-headlines?country=$country&category=$category&apiKey=$apiKey")
+                .body()
+        return response.articles
+    }
+
+}
