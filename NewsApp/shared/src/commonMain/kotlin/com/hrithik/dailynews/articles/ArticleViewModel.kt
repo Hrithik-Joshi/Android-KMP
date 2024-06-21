fe888fa0a7889cb6c47a844fdb48411a537b80a1
Hrithik-Joshi <84170164+Hrithik-Joshi@users.noreply.github.com>
2024-06-21 14:18:12 +0530

    instantiated service and use case class in viewmodel

--- NewsApp/shared/src/commonMain/kotlin/com/hrithik/dailynews/articles/ArticleViewModel.kt
+++ NewsApp/shared/src/commonMain/kotlin/com/hrithik/dailynews/articles/ArticleViewModel.kt
@@ -1,17 +1,22 @@
 package com.hrithik.dailynews.articles
 
 import com.hrithik.dailynews.BaseViewModel
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import kotlinx.serialization.json.Json
 
-class  ArticleViewModel : BaseViewModel() {
+class ArticleViewModel : BaseViewModel() {
 
     /* It holds an initial state of ArticleState.
     MutableStateFlow is a state holder observable that holds a single value
     and can be updated.
     */
-    private val _articleState: MutableStateFlow<ArticleState> = MutableStateFlow(ArticleState(loading = true))
+    private val _articleState: MutableStateFlow<ArticleState> =
+        MutableStateFlow(ArticleState(loading = true))
 
     /* This is a public, read-only StateFlow property that exposes the state to observers.
     It returns the _articleState which means any changes to _articleState
@@ -19,18 +24,32 @@ class  ArticleViewModel : BaseViewModel() {
     */
     val articleState: StateFlow<ArticleState>
         get() = _articleState
+
+    private val useCase: ArticlesUseCase
+
+    init {
+        val httpClient = HttpClient {
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                })
+            }
+        }
+
+        val service = ArticlesService(httpClient)
+
+        useCase = ArticlesUseCase(service)
+
+        getArticles()
+    }
+
     fun getArticles(forceFetch: Boolean = false) {
         scope.launch {
-            _articleState.emit(
-                ArticleState(
-                    loading = true,
-                    articles = _articleState.value.articles
-                )
-            )
+            val fetchedArticles = useCase.getArticles(forceFetch)
 
-//            val fetchedArticles = useCase.getArticles(forceFetch)
-//
-//            _articleState.emit(ArticleState(articles = fetchedArticles))
+            _articleState.emit(ArticleState(articles = fetchedArticles))
         }
     }
 }
